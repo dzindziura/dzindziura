727533ad3750172fda0d005c2854bece3bc2dcc6
Roman <romandzindzyura@gmail.com>
2023-03-05 00:55:33 +0200

    remove header

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,8 @@
 import "./App.css";
-import Header from "./components/header/header";
 import Kanban from "./components/module/Kanban/Kanban";
 const App = () => {
   return (
     <>
-      <Header />
       <Kanban />
     </>
   );
